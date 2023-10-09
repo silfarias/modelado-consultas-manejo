1a5f07a60d18c8ccf7c558dff8cff6d11d493df9
silfarias17 <silvifarias17@gmail.com>
2023-10-09 09:42:44 -0300

    cambio en las rutas

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 const booksRouter = Router();
 import { deleteBook, getAllBooks, getBook, listOfBooks, newBook, updateBook } from "../controllers/books.controller.js";
 
-booksRouter.get('/', getAllBooks )
+
+booksRouter.get('/', listOfBooks );
+
+booksRouter.get('/getAllBooks', getAllBooks )
 
 booksRouter.get('/:id', getBook );
-
-booksRouter.get('/listadoLibros', listOfBooks );
 
 booksRouter.post('/', newBook );
 
